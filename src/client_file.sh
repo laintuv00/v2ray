bed26950fd4fbeba82ce125aa130fcac90993e5d
laintuv00 <laintuv00@gmail.com>
2020-08-23 09:39:41 +0700

    Update client_file.sh

--- src/client_file.sh
+++ src/client_file.sh
@@ -4,11 +4,11 @@ _get_client_file() {
     local _link="$(cat $v2ray_client_config | tr -d [:space:] | base64 -w0)"
     local link="https://233boy.github.io/tools/json.html#${_link}"
     echo
-    echo "---------- V2Ray 客户端配置文件链接 -------------"
+    echo "---------- V2Ray Client configuration file link -------------"
     echo
     echo -e ${cyan}$link${none}
     echo
-    echo " V2Ray 客户端使用教程: https://233v2.com/post/4/"
+    echo " V2Ray Client tutorial: https://233v2.com/post/4/"
     echo
     echo
 }
