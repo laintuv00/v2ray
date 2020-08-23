5ad9da114dedf15ec465179c1402ae44384634b1
laintuv00 <laintuv00@gmail.com>
2020-08-23 09:45:28 +0700

    Update bt.sh

--- src/bt.sh
+++ src/bt.sh
@@ -1,19 +1,19 @@
 _ban_bt_main() {
 	if [[ $ban_bt ]]; then
-		local _info="$green已开启$none"
+		local _info="$green Activated $none"
 	else
-		local _info="$red已关闭$none"
+		local _info="$red Not running $none"
 	fi
 	_opt=''
 	while :; do
 		echo
-		echo -e "$yellow 1. $none开启 BT 屏蔽"
+		echo -e "$yellow 1. $none Turn on BT blocking"
 		echo
-		echo -e "$yellow 2. $none关闭 BT 屏蔽"
+		echo -e "$yellow 2. $none Turn off BT blocking"
 		echo
-		echo -e "当前 BT 屏蔽状态: $_info"
+		echo -e "Current BT blocking status: $_info"
 		echo
-		read -p "$(echo -e "请选择 [${magenta}1-2$none]:")" _opt
+		read -p "$(echo -e "Please choose [${magenta}1-2$none]:")" _opt
 		if [[ -z $_opt ]]; then
 			error
 		else
@@ -21,12 +21,12 @@ _ban_bt_main() {
 			1)
 				if [[ $ban_bt ]]; then
 					echo
-					echo -e " 大胸弟...难不成你没有看到 (当前 BT 屏蔽状态: $_info) 这个帅帅的提示么.....还开启个鸡鸡哦"
+					echo -e " Big boss...Is it possible that you didn't see (current BT blocking status: $_info) this handsome reminder...and turn on it"
 					echo
 				else
 					echo
 					echo
-					echo -e "$yellow  BT 屏蔽 = $cyan开启$none"
+					echo -e "$yellow  BT blocking = $cyan Turn on $none"
 					echo "----------------------------------------------------------------"
 					echo
 					pause
@@ -35,7 +35,7 @@ _ban_bt_main() {
 					config
 					echo
 					echo
-					echo -e "$green  BT 屏蔽已开启...如果出现异常..那就关闭它咯$none"
+					echo -e "$green  BT blocking is turned on...If something goes wrong... then close it$none"
 					echo
 				fi
 				break
@@ -44,7 +44,7 @@ _ban_bt_main() {
 				if [[ $ban_bt ]]; then
 					echo
 					echo
-					echo -e "$yellow  BT 屏蔽 = $cyan关闭$none"
+					echo -e "$yellow  BT blocking = $cyan Turn off $none"
 					echo "----------------------------------------------------------------"
 					echo
 					pause
@@ -53,11 +53,11 @@ _ban_bt_main() {
 					config
 					echo
 					echo
-					echo -e "$red  BT 屏蔽已关闭...不过你也可以随时重新开启 ...只要你喜欢$none"
+					echo -e "$red  BT blocking is turned off...but you can turn it back on anytime...as long as you like $none"
 					echo
 				else
 					echo
-					echo -e " 大胸弟...难不成你没有看到 (当前 BT 屏蔽状态: $_info) 这个帅帅的提示么.....还关闭个鸡鸡哦"
+					echo -e " Big guy...Is it possible that you didn't see (current BT blocking status: $_info) this handsome reminder... and also turn off it"
 					echo
 				fi
 				break
